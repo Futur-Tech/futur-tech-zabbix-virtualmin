8e15e52e4f2aa29362e34d8e30b1b8aab91da174
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2022-03-01 18:41:06 +0100

    Update bkp-discovery.sh

--- bin/bkp-discovery.sh
+++ bin/bkp-discovery.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 virtualmin list-scheduled-backups --multiline \
-    | sed -E "s/^[^\ ]+/},{ \"INDEX\":\"\0\", \"DETAILS\": \{/g"    `# Add INDEX key and },{` \
-    | sed -E "s/^[\ ]{4}(.+): (.+)$/\"\1\" : \"\2\",/g"             `# Put key and value with " "` \
-    | tr '\n' ' '                                                   `# Remove all line returns` \
-    | sed -E "s/, \},\{/ \} \} , \{/g"                              `# Add space in },{` \
-    | sed -E "s/^\},(\{)/\[ \1/g"                                   `# Remove the }, at the beginning of the line and add [` \
-    | sed -E "s/, $/ \} \} \]/g"                                    `# Remove the , in the end and replace by ]` \
-    | python -m json.tool                                           `# Prettify the JSON`
+    | sed -E 's|^[^ ]+|}},{ "INDEX":"\0", "DETAILS": {|g'   `# Add INDEX key and }},{` \
+    | sed -E 's|^[ ]{4}(.+): (.+)$|"\1" : "\2",|g'          `# Put key and value with " "` \
+    | tr '\n' ' '                                           `# Remove all line returns` \
+    | sed 's|, }},{| }},{|g'                                `# Remove , at the end of the DETAILS array` \
+    | sed -E 's|^\}\},\{|[{|g'                              `# Remove the }},{ at the beginning of the line and add [{` \
+    | sed -E 's|, $|}}]|g'                                  `# Remove the , in the end and replace by }}]` \
+    | jq                                                    `# Process and Prettify the JSON`
