2e32e32c87424b92c947453dabf7f4702896825d
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2022-03-03 21:49:22 +0100

    Update to Zabbix Agent 2

--- deploy.sh
+++ deploy.sh
@@ -5,35 +5,32 @@ source "$(dirname "$0")/ft-util/ft_util_inc_var"
 APP_NAME="futur-tech-zabbix-virtualmin"
 REQUIRED_PKG_ARR=( "at" "jq" )
 
-ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agentd.conf.d"
 BIN_DIR="/usr/local/bin/${APP_NAME}"
 SRC_DIR="/usr/local/src/${APP_NAME}"
 SUDOERS_ETC="/etc/sudoers.d/${APP_NAME}"
 
+# Checking which Zabbix Agent is detected and adjust include directory
+$(which zabbix_agent2 >/dev/null) && ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agent2.d"
+$(which zabbix_agentd >/dev/null) && ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agentd.conf.d"
+if [ ! -d "${ZBX_CONF_AGENT_D}" ] ; then $S_LOG -s crit -d $S_NAME "${ZBX_CONF_AGENT_D} Zabbix Include directory not found" ; exit 10 ; fi
+
 $S_LOG -d $S_NAME "Start $S_DIR_NAME/$S_NAME $*"
 
-echo
-echo "------------------------------------------"
-echo "  INSTALL NEEDED PACKAGES & FILES"
-echo "------------------------------------------"
-echo
+echo "
+  INSTALL NEEDED PACKAGES & FILES
+------------------------------------------"
 
 $S_DIR_PATH/ft-util/ft_util_pkg -u -i ${REQUIRED_PKG_ARR[@]} || exit 1
 
-if [ ! -d "${ZBX_CONF_AGENT_D}" ] ; then mkdir "${ZBX_CONF_AGENT_D}" ; $S_LOG -s $? -d $S_NAME "Creating ${ZBX_CONF_AGENT_D} returned EXIT_CODE=$?" ; fi
 if [ ! -d "${BIN_DIR}" ] ; then mkdir "${BIN_DIR}" ; $S_LOG -s $? -d $S_NAME "Creating ${BIN_DIR} returned EXIT_CODE=$?" ; fi
 
 $S_DIR/ft-util/ft_util_file-deploy "$S_DIR/bin/" "${BIN_DIR}"
 $S_DIR/ft-util/ft_util_file-deploy "$S_DIR/etc.zabbix/${APP_NAME}.conf" "${ZBX_CONF_AGENT_D}/${APP_NAME}.conf"
 
-echo
-echo "------------------------------------------"
-echo "  SETUP SUDOERS FILE"
-echo "------------------------------------------"
-echo
+echo "
+  SETUP SUDOERS FILE
+------------------------------------------"
 
-$S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
-$S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==== SUDOERS CONFIGURATION ===="
 $S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
 
 echo "Defaults:zabbix !requiretty" | sudo EDITOR='tee' visudo --file=$SUDOERS_ETC &>/dev/null
@@ -47,15 +44,12 @@ echo "zabbix ALL=(ALL) NOPASSWD:${BIN_DIR}/domain-info.sh *" | sudo EDITOR='tee 
 cat $SUDOERS_ETC | $S_LOG -d "$S_NAME" -d "$SUDOERS_ETC" -i 
 
 $S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
-$S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
 
-echo
-echo "------------------------------------------"
-echo "  RESTART ZABBIX LATER"
-echo "------------------------------------------"
-echo
+echo "
+  RESTART ZABBIX LATER
+------------------------------------------"
 
-echo "service zabbix-agent restart" | at now + 1 min &>/dev/null ## restart zabbix agent with a delay
+echo "systemctl restart zabbix-agent*" | at now + 1 min &>/dev/null ## restart zabbix agent with a delay
 $S_LOG -s $? -d "$S_NAME" "Scheduling Zabbix Agent Restart"
 
 $S_LOG -d "$S_NAME" "End $S_NAME"
