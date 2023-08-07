9300ce4ee2c9f0e35bc7820bfcdac3acc7747f53
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2023-08-07 15:08:32 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -41,8 +41,8 @@ echo "
 letsencrypt="/etc/ssl/pfsense-acme/letsencrypt.all.pem"
 
 if [ -e "$letsencrypt" ]; then
-  run_cmd_log ln -sf "$letsencrypt" /etc/webmin/miniserv.pem
-  run_cmd_log webmin set-config --option keyfile --value /etc/webmin/miniserv.pem
+  run_cmd_log webmin set-config --option keyfile --value /etc/ssl/pfsense-acme/letsencrypt.all.pem
+  run_cmd_log webmin set-config --option certfile
 fi
 
 echo "
