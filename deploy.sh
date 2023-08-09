d817966e056eb8891383dc20abcc84246cfbcc00
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2023-08-09 18:35:50 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,8 +14,8 @@ sudoers_etc="/etc/sudoers.d/${app_name}"
 $(which zabbix_agent2 >/dev/null) && ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agent2.d"
 $(which zabbix_agentd >/dev/null) && ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agentd.conf.d"
 if [ ! -d "${ZBX_CONF_AGENT_D}" ]; then
-  $S_LOG -s crit -d $S_NAME "${ZBX_CONF_AGENT_D} Zabbix Include directory not found"
-  exit 10
+    $S_LOG -s crit -d $S_NAME "${ZBX_CONF_AGENT_D} Zabbix Include directory not found"
+    exit 10
 fi
 
 $S_LOG -d $S_NAME "Start $S_DIR_NAME/$S_NAME $*"
@@ -27,7 +27,7 @@ echo "
 $S_DIR_PATH/ft-util/ft_util_pkg -u -i ${required_pkg_arr[@]} || exit 1
 
 if [ ! -d "${bin_dir}" ]; then
-  run_cmd_log mkdir "${bin_dir}"
+    run_cmd_log mkdir "${bin_dir}"
 fi
 
 $S_DIR/ft-util/ft_util_file-deploy "$S_DIR/bin/" "${bin_dir}"
@@ -37,14 +37,31 @@ echo "
   SETUP VIRTUALMIN
 ------------------------------------------"
 
-# Define the path to the Let's Encrypt SSL certificate
+# Run webmin command
+function run_webmin_cmd_log() {
+    $@ 2>&1 | $S_LOG -s debug -d "$S_NAME|[${*}]" -i
+    local exit_code=${PIPESTATUS[0]}
+    if [ $exit_code -eq 0 ] || [ $exit_code -eq 1 ]; then
+        # Exit Codes:
+        # 0 on successfully replacing a config variable
+        # 1 on successfully adding a new config variable (the specified option did
+        # not already exist in the file, and was added)
+        # >1 on error
+
+        $S_LOG -d "$S_NAME" "[${*}] successful"
+    else
+        $S_LOG -s crit -d "$S_NAME" "[${*}] failed with EXIT_CODE=${exit_code}"
+        exit $exit_code
+    fi
+}
+
+# Use pfSense ACME generated certificate (or self signed if not deployed yet)
 letsencrypt="/etc/ssl/pfsense-acme/letsencrypt.all.pem"
 
 if [ -e "$letsencrypt" ]; then
-  run_cmd_log_noexit webmin set-config --option keyfile --value /etc/ssl/pfsense-acme/letsencrypt.all.pem
-  run_cmd_log_noexit webmin set-config --option certfile
+    run_webmin_cmd_log webmin set-config --option keyfile --value /etc/ssl/pfsense-acme/letsencrypt.all.pem
+    run_webmin_cmd_log webmin set-config --option certfile --value \"\"
 fi
-
 echo "
   SETUP SUDOERS FILE
 ------------------------------------------"
